3b405ba2b802e322cee3f3d523571456a4e26cfa
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-02-28 12:07:18 +0600

    Configuring the admin-panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,24 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Post, Author, Tag
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdminForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple()
+        }
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
