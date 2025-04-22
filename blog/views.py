4d427a49940a2a15fa7a28cdc4bef244d91ee45b
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-04-22 14:46:58 +0600

    Constructing page: Stored-Posts

--- blog/views.py
+++ blog/views.py
@@ -65,6 +65,23 @@ class SinglePostView(View):
         return render(request, "blog/post-detail.html", context)
 
 class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+    
+
     def post(self, request):
         stored_posts = request.session.get("stored_posts")
 
