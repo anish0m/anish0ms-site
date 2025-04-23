0d3d2f356bf79b41c88e9847262f09bb00650fb1
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-04-23 12:21:43 +0600

    Removing Posts from Read-Later on clicking Button

--- blog/views.py
+++ blog/views.py
@@ -76,18 +76,19 @@ class SinglePostView(View):
 
 class ReadLaterView(View):
     def get(self, request):
-        stored_posts = request.session.get("stored_posts")
+        stored_posts = request.session.get("stored_posts", [])  # Default to an empty list
 
-        context = {}
-
-        if stored_posts is None or len(stored_posts) == 0:
-            context["posts"] = []
-            context["has_posts"] = False
+        if not stored_posts:  # Check if the list is empty
+            context = {
+                "posts": [],
+                "has_posts": False,
+            }
         else:
             posts = Post.objects.filter(id__in=stored_posts)
-
-            context["posts"] = posts
-            context["has_posts"] = True
+            context = {
+                "posts": posts,
+                "has_posts": True,
+            }
 
         return render(request, "blog/stored-posts.html", context)
     
@@ -102,6 +103,9 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+        
+        request.session["stored_posts"] = stored_posts
 
         return HttpResponseRedirect("/")
