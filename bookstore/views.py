2a8bec7fd53d841178aa71a367ca2a3f7bb0862b
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-04-25 13:47:26 +0600

    Add models for Book, Author, and Tag with initial migration

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from .models import Book
 
 # Create your views here.
 
 
-class StartingPageView(TemplateView):
+class StartingPageView(ListView):
     template_name = "bookstore/index.html"
+    model = Book
+    ordering = ["-date"]
+    context_object_name = "books"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+
+        return data
