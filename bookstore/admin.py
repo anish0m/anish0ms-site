b98e9c29db39cb17c15bb6e12990b922699c907c
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-04-25 17:13:00 +0600

    Add SubBook model and integrate with Book admin interface; update migrations and model fields

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.contrib import admin
 
-from .models import Book, Author, Tag
+from .models import SubBook, Book, Author, Tag
 
 # Register your models here.
 
+class SubBookInline(admin.TabularInline):
+    model = SubBook
+    extra = 1
+    fields = ("title", "description")
 class BookAdminForm(forms.ModelForm):
     class Meta:
         model = Book
@@ -12,12 +16,12 @@ class BookAdminForm(forms.ModelForm):
         widgets = {
             "tags": forms.CheckboxSelectMultiple()
         }
-
 class BookAdmin(admin.ModelAdmin):
     form = BookAdminForm
     list_filter = ("author", "tags", "date",)
     list_display = ("title", "date", "author",)
     prepopulated_fields = {"slug": ("title",)}
+    inlines = [SubBookInline]
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
--- bookstore/models.py
+++ bookstore/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -24,14 +25,28 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="posts", null=True)
+    image = models.ImageField(upload_to="books", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
-        Author, null=True, on_delete=models.SET_NULL, related_name="posts")
+        Author, null=True, on_delete=models.SET_NULL, related_name="books")
+    no_of_books = models.IntegerField(default=0, editable=False)
     tags = models.ManyToManyField(Tag)
 
+
+    def __str__(self):
+        return self.title
+    
+class SubBook(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="sub_books")
+    title = models.CharField(max_length=150)
+    description = models.TextField(validators=[MinLengthValidator(10)], null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.description and self.book:
+            self.description = self.book.content
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
