8f41f60ec1e0247deb9f05ed988728134a7f2f0d
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-05-06 15:07:40 +0600

    Admin access

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,27 @@
+from django import forms
 from django.contrib import admin
 
+from .models import Ingredients, Recipe, SearchFilter
+
 # Register your models here.
+
+class IngredientsInline(admin.TabularInline):
+    model = Ingredients
+    extra = 1
+    fields = ("name", "amount")
+class RecipeAdminForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = "__all__"
+        widgets = {
+            "search_filter": forms.CheckboxSelectMultiple()
+        }
+class RecipeAdmin(admin.ModelAdmin):
+    form = RecipeAdminForm
+    list_filter = ("search_filter", "date",)
+    list_display = ("title", "date",)
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [IngredientsInline]
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(SearchFilter)  
