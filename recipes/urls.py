4c0a3750a30ca973c4a0d59d096dc8c8ca9e4c25
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-05-06 15:54:10 +0600

    Added necessary URL with slug

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name="starting-page-recipes"),
     path("recipes", views.AllRecipesView.as_view(), name="recipes-page"),
-    path("recipes/<slug:slug>", views.SinglerecipeView.as_view(), name="recipe-detail-page"),
+    path("recipes/<slug:slug>", views.SingleRecipeView.as_view(), name="recipe-detail-page"),
 ]
