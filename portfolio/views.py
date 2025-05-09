1ad410eaf1c74dba9459c5b93d10bcc2f89e2cf6
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-05-09 13:44:31 +0600

    GET method to render template

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,10 +4,6 @@ from django.views import View
 # Create your views here.
 class PortfolioView(View):
     template_name = "portfolio/index.html"
-    # model = Contents
-    # ordering = ["-date"]
-    # context_object_name = "contents"
 
-    # def get_queryset(self):
-    #     queryset = super().get_queryset()
-    #     return queryset
+    def get(self, request):
+        return render(request, self.template_name)
