ae4bf0cf70137b1fcf240402ab72bc6676502b01
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-05-13 19:10:40 +0600

    Create model to handle Projects

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    CATEGORY_CHOICES = [
+        ('web-dev', 'Development'),
+        ('web-des', 'Research'),
+        ('dig-mar', 'Machine Learning'),
+    ]
+
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=300)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    github_link = models.URLField()
+    filter_tag = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return self.title
