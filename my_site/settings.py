b8d289a3dac81353621f169a69b1cbc3c88295f3
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-06 13:38:27 +0600

    Add Cloudinary integration and update requirements and render.yaml

--- my_site/settings.py
+++ my_site/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'bookstore',
     'recipes',
     'portfolio',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +141,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/files/"
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
